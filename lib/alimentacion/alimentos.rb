0ebbbfeb9fa746448628d7828be2ddde719cbcae
David Hernandez <alu0101048239@ull.edu.es>
2019-11-13 20:22:48 +0000

    Alimentos   Probando a crear objetos (FAILED - 1)
    Failures:
    
      1) Alimentos Probando a crear objetos
         Failure/Error: @alimento1 = Alimentos.new("Chocolate",5.3,47.0,30.0,2.3,3.4)
    
         ArgumentError:
           wrong number of arguments (given 6, expected 0)
         # ./spec/alimentacion_spec.rb:3:in `initialize'
         # ./spec/alimentacion_spec.rb:3:in `new'
         # ./spec/alimentacion_spec.rb:3:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00179 seconds (files took 0.1459 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/alimentacion_spec.rb:2 # Alimentos Probando a crear objetos

--- lib/alimentacion/alimentos.rb
+++ lib/alimentacion/alimentos.rb
@@ -1,2 +1,6 @@
 class Alimentos
+	attr_reader :nombre,:proteinas,:hidratos,:lipidos,:gei,:terreno
+	def initialize(nombre,proteinas,hidratos,lipidos,gei,terreno)
+		@nombre,@proteinas,@hidratos,@lipidos,@gei,@terreno = nombre,proteinas,hidratos,lipidos,gei,terreno
+	end
 end
--- spec/alimentacion_spec.rb
+++ spec/alimentacion_spec.rb
@@ -1,3 +1,11 @@
 RSpec.describe Alimentos do
+	it "Probando a crear objetos" do
+		@alimento1 = Alimentos.new("Chocolate",5.3,47.0,30.0,2.3,3.4)
+		@alimento2 = Alimentos.new("Cerdo",21.5,0.0,6.3,7.6,11.0)
+		@alimento3 = Alimentos.new("Pollo",20.6,0.0,5.6,5.7,7.1)
+		@alimento4 = Alimentos.new("Queso",25.0,1.3,33.0,11.0,41.0)
+		@alimento5 = Alimentos.new("Huevos",13.0,1.1,11.0,4.2,5.7)
+
+	end
   
 end
