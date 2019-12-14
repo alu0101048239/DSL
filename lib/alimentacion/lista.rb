4a6a1b929e9e286f510a36f024b5122e6374178f
David Hernandez <alu0101048239@ull.edu.es>
2019-12-14 23:12:45 +0000

    Comentando la clase Lista

--- lib/alimentacion/lista.rb
+++ lib/alimentacion/lista.rb
@@ -1,7 +1,17 @@
+# encoding: utf-8
+# Esta clase implementa una lista doblemente enlazada.
+# Se desarrollan los métodos para insertar y extraer nodos por la cabeza y por la cola, e imprimir la lista completa.
+# Se utiliza una estructura para representar los nodos, con los valores value, next y prev.
+#
+# Author::      David Hernández Suárez (mailto:david@alimentos)
+# Copyright::   Cretive Commons
+# License::     Distributes under the same terms as Ruby
+
 Nodo = Struct.new(:value, :next, :prev)
 class Lista
 	include Enumerable
 
+	# Se utiliza el módulo Enumerable, para enumerar cualquier lista enlazada.
         def each
                 aux = @head
                 while !aux.nil?
@@ -11,11 +21,14 @@ class Lista
         end
 
 	attr_accessor :head,:tail
+
+	# Se asignan la cabecera de la lista y la cola de la lista. Inicialmente apuntan a nil.
         def initialize(head,tail)
                 @head = nil
                 @tail = nil
         end
 
+	# Se inserta un nodo por la cabeza de la lista
 	def insert_head(value)
                 if @head.nil?
                         @head = Nodo.new(value, nil, nil)
@@ -26,6 +39,7 @@ class Lista
                 end
         end
 
+	# Se inserta un nodo por la cola de la lista
 	def insert_tail(value)
                 if @head.nil?
                         @head = Nodo.new(value, nil, nil)
@@ -35,7 +49,8 @@ class Lista
                         @tail = @tail[:next]
                 end
         end
-	
+
+	# Se extrae el nodo situado en la cabeza de la lista
 	def extract_head()
                 if @head.nil?
                         raise RuntimeError, "La lista está vacía"
@@ -50,6 +65,7 @@ class Lista
                 end
         end
 
+	# Se extrae el nodo situado en la cola de la lista
 	def extract_tail()
                 if @head.nil?
                         raise RuntimeError, "La lista está vacía"
@@ -63,12 +79,14 @@ class Lista
                 end
         end
 
+	# Se insertan por la cola un conjunto de nodos
 	def insert(nodos)
             for i in (0.. nodos.size-1)
                 insert_tail(nodos[i])
             end
         end
 
+	# Se visualiza la lista
 	def imprimir
                 aux = @head
                 lista = []
@@ -80,6 +98,7 @@ class Lista
 
         end
 
+	# Calcula el total de gases anuales que tiene la lista de alimentos
 	def dieta_gases_anuales(gramos)
                 aux = @head
                 suma_gases = 0
@@ -93,11 +112,13 @@ class Lista
 
         end
 
+	# Calcula el total de gases diarios que tiene la lista de alimentos
 	def dieta_gases_diarios(gramos)
                 gases_diarios = dieta_gases_anuales(gramos)/365
                 return gases_diarios.round(3)
         end
 
+	# Calcula el total de uso del terreno que tiene la lista de alimentos
 	def dieta_terreno(gramos)
 
                 aux = @head
