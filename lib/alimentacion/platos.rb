ed787e3d8145fee7f61439c140a042cc80c7875b
David Hernandez <alu0101048239@ull.edu.es>
2019-12-14 22:32:29 +0000

    Comentando la clase Plato

--- lib/alimentacion/platos.rb
+++ lib/alimentacion/platos.rb
@@ -1,16 +1,28 @@
+# encoding: utf-8
+# Esta clase contiene información acerca de los distintos alimentos que forman un plato.
+# Se calcula el porcentaje de proteínas, hidratos y lípidos que contiene cada conjunto de alimentos, así como su valor energético.
+#
+# Author::      David Hernández Suárez (mailto:alu0101048239@ull.edu.es)
+# Copyright::   Cretive Commons
+# License::     Distributes under the same terms as Ruby
+
 class Plato 
 	attr_reader :nombre_plato, :lista_alimentos, :lista_gramos
 	include Comparable
+
+	# Se utiliza el módulo Comparable, comparando los alimentos en base a su porcentaje de proteínas.
         def <=>(anOther)
                 self.porcentaje_proteinas <=> anOther.porcentaje_proteinas
         end
 
+	# Se asignan el nombre del plato, la lista de alimentos que lo compone y los gramos de cada alimento.
         def initialize(nombre_plato, lista_alimentos, lista_gramos)
                 @nombre_plato = nombre_plato
                 @lista_alimentos = lista_alimentos
                 @lista_gramos = lista_gramos
         end
 
+	# Se calcula el porcentaje de proteínas total del plato
 	def porcentaje_proteinas
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -27,6 +39,7 @@ class Plato
 
         end
 
+	# Se calcula el porcentaje de hidratos de carbono total del plato
 	 def porcentaje_hidratos
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -43,6 +56,7 @@ class Plato
 
         end
 
+	# Se calcula el porcentaje de lípidos total del plato
 	def porcentaje_lipidos
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -59,6 +73,7 @@ class Plato
 
         end
 
+	# Se calcula el valor calórico total del plato
 	def valor_calorico
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -75,6 +90,7 @@ class Plato
                 return calorias.round(2)
         end
 	
+	# Se obtiene el plato formateado
 	def to_s
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -93,19 +109,31 @@ class Plato
 
 end
 
+# encoding: utf-8
+# Esta clase, que hereda de la clase Plato, calculará las emisiones diarias y anuales de los gases de efecto invernadero, y el uso del terreno# en referencia a los alimentos que componen cada plato.
+#
+# Author::      David Hernández Suárez (mailto:alu0101048239@ull.edu.es)
+# Copyright::   Cretive Commons
+# License::     Distributes under the same terms as Ruby
+
+
 class Eficiencia_Energetica < Plato
 
 	attr_reader :nombre_plato, :lista_alimentos, :lista_gramos
         include Comparable
-        def <=>(anOther)
+        
+	# Se utiliza el módulo Comparable, comparando los alimentos en base a su huella nutricional.
+	def <=>(anOther)
                #self.uso_terreno <=> anOther.uso_terreno
 		self.huella_nutricional <=> anOther.huella_nutricional
         end
 
+	# Se asignan el nombre del plato, la lista de alimentos que lo contienen y la lista de gramos de cada alimento.
         def initialize(nombre_plato, lista_alimentos, lista_gramos)
                 super(nombre_plato, lista_alimentos, lista_gramos)
         end
 
+	# Se calculan las emisiones anuales de gases de efecto invernadero que produce el plato
         def emisiones_gases_anuales
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -119,11 +147,13 @@ class Eficiencia_Energetica < Plato
                 return suma_gases_anuales.round(2)
         end
 
+	# Se calculan las emisiones diarias de gases de efecto invernadero que produce el plato
 	def emisiones_gases_diarios
                 suma = (emisiones_gases_anuales / 365)
                 return suma.round(3)
         end
 
+	# Se calcula el uso del terreno anual que produce el plato
 	def uso_terreno
                 aux = @lista_alimentos.head
                 aux2 = @lista_gramos.head
@@ -137,11 +167,13 @@ class Eficiencia_Energetica < Plato
                 return suma_terreno.round(2)
         end
 
+	# Se obtiene la información de gases y terreno formateada
 	def to_s
                 "( #{self.emisiones_gases_anuales}, #{self.emisiones_gases_diarios}, #{self.uso_terreno} )"
 
         end
 
+	# Se calcula la huella nutricional del plato
 	def huella_nutricional
                 if (valor_calorico < 670)
                         aux = 1
