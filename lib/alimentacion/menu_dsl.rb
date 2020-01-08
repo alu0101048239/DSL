595bb2d0e8883b337f607a8c509c3a91087fa4bc
David Hernandez <alu0101048239@ull.edu.es>
2020-01-08 19:53:13 +0000

    Menu_DSL   Probando el DLS     Probando la clase Menu_DSL (FAILED - 1)
    Failures:
    
      1) Menu_DSL Probando el DLS Probando la clase Menu_DSL
         Failure/Error: menu = Menu_DSL.new("Combinado nº 1")
    
         ArgumentError:
           wrong number of arguments (given 1, expected 0)
         # ./spec/alimentacion_spec.rb:752:in `initialize'
         # ./spec/alimentacion_spec.rb:752:in `new'
         # ./spec/alimentacion_spec.rb:752:in `block (3 levels) in <top (required)>'
    
    Finished in 0.03474 seconds (files took 0.21166 seconds to load)
    61 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/alimentacion_spec.rb:751 # Menu_DSL Probando el DLS Probando la clase Menu_DSL

--- lib/alimentacion/menu_dsl.rb
+++ lib/alimentacion/menu_dsl.rb
@@ -1,2 +1,17 @@
 class Menu_DSL
+attr_reader :nombre_menu, :lista_platos, :precio_menu
+
+	def initialize (nombre_menu, &block)
+		#@nombre_menu = nombre_menu
+		@lista_platos = []
+		@precios_menu = []
+
+		if block_given?
+                        if block.arity == 1
+                                yield self
+                        else
+                                instance_eval(&block)
+                        end
+                end
+	end
 end
