042694b0e022833c5781c3db295df951d2f4fbee
David Hernandez <alu0101048239@ull.edu.es>
2020-01-08 16:02:47 +0000

    Plato_DSL   Probando el DSL     Probando la clase Plato_DSL (FAILED - 1)
    Failures:
    
      1) Plato_DSL Probando el DSL Probando la clase Plato_DSL
         Failure/Error: plato = Plato_DSL.new("Hamburguesa")
    
         ArgumentError:
           wrong number of arguments (given 1, expected 0)
         # ./spec/alimentacion_spec.rb:657:in `initialize'
         # ./spec/alimentacion_spec.rb:657:in `new'
         # ./spec/alimentacion_spec.rb:657:in `block (3 levels) in <top (required)>'
    
    Finished in 0.02978 seconds (files took 0.19585 seconds to load)
    60 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/alimentacion_spec.rb:656 # Plato_DSL Probando el DSL Probando la clase Plato_DSL

--- lib/alimentacion/plato_dsl.rb
+++ lib/alimentacion/plato_dsl.rb
@@ -1,3 +1,23 @@
 class Plato_DSL
 
+	attr_reader :nombre_plato, :lista_alimentos, :lista_gramos
+        include Comparable
+        def <=>(anOther)
+                self.porcentaje_proteinas <=> anOther.porcentaje_proteinas
+        end
+
+        def initialize(nombre_plato, &block)
+                @nombre_plato = nombre_plato
+                @lista_alimentos = []
+                @lista_gramos = []
+
+                if block_given?
+                        if block.arity == 1
+                                yield self
+                        else
+                                instance_eval(&block)
+                        end
+                end
+        end
+
 end
